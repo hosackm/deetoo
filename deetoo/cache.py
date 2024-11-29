480815eee580092877ac94c77f62e157e4fa3c43
Matt Hosack <hosack.matt@gmail.com>
2024-11-29 10:46:58 +0100

    Change cache layout to be more ergonomic. Adding more files to gitignore

--- deetoo/cache.py
+++ deetoo/cache.py
@@ -1,11 +1,10 @@
-from fastapi import FastAPI, Request
-from sqlmodel.ext.asyncio.session import AsyncSession
 from contextlib import asynccontextmanager
-
 from deetoo.models import sqlite_url
 from deetoo.models.item import Item, UniqueItem, SetItem, Set
 from deetoo.models.serialize import ItemRead, UniqueItemRead, SetItemRead, SetRead
+from fastapi import FastAPI, Request
 from sqlmodel import select
+from sqlmodel.ext.asyncio.session import AsyncSession
 from sqlalchemy.orm import joinedload
 from sqlalchemy.ext.asyncio import create_async_engine
 
@@ -13,75 +12,63 @@ import logging
 
 logger = logging.getLogger("uvicorn")
 
-cache = {}
-
 
 def cache_lookup(request: Request):
-    key = request.url.path
-    if key in cache:
-        logger.info(f"Cache hit: {key}")
-        return cache[key]
+    if request.url.path in request.app.cache:
+        logger.info(f"Cache hit: {request.url.path}")
+        return request.app.cache[request.url.path]
+
     return None
 
 
 @asynccontextmanager
-async def lifespan(_app: FastAPI):
+async def lifespan(app: FastAPI):
     logger.info(f"Pre-loading cache with values from: {sqlite_url}")
-    await load_cache()
+    app.cache = Cache()
+    await app.cache.load_cache()
     yield
 
 
-async def load_cache():
-    engine = create_async_engine(sqlite_url)
-    session = AsyncSession(engine)
+class Cache:
+    def __init__(self):
+        self.store = {}
 
-    global cache
-    await load_base_items(session, cache)
-    await load_unique_items(session, cache)
-    await load_set_items(session, cache)
-    await load_sets(session, cache)
+    def __getitem__(self, key, default=None):
+        return self.store.get(key, default)
 
-    await session.close()
+    def __contains__(self, key):
+        return key in self.store
 
+    async def load_cache(self):
+        self.engine = create_async_engine(sqlite_url)
+        self.session = AsyncSession(self.engine)
+        await self.load()
+        await self.session.close()
 
-async def load_base_items(session, cache):
-    results = (await session.exec(select(Item))).all()
-    base_results = [ItemRead.model_validate(r) for r in results]
-    for r in base_results:
-        cache[f"/base_items/{r.id}"] = r
-    cache["/base_items"] = base_results
+    async def load(self):
+        queries = [
+            ("/base_items", select(Item), ItemRead),
+            (
+                "/unique_items",
+                select(UniqueItem).options(joinedload(UniqueItem.base_item)),
+                UniqueItemRead,
+            ),
+            (
+                "/set_items",
+                select(SetItem).options(
+                    joinedload(SetItem.base_item), joinedload(SetItem.set)
+                ),
+                SetItemRead,
+            ),
+            ("/sets", select(Set), SetRead),
+        ]
 
-
-async def load_unique_items(session, cache):
-    results = (
-        await session.exec(select(UniqueItem).options(joinedload(UniqueItem.base_item)))
-    ).all()
-    unique_results = [UniqueItemRead.model_validate(r) for r in results]
-    for r in unique_results:
-        cache[f"/unique_items/{r.id}"] = r
-    cache["/unique_items"] = unique_results
-
-
-async def load_set_items(session, cache):
-    results = (
-        await session.exec(
-            select(SetItem).options(
-                joinedload(SetItem.base_item),
-                joinedload(SetItem.set),
-            )
-        )
-    ).all()
-
-    set_results = [SetItemRead.model_validate(r, from_attributes=True) for r in results]
-    for r in set_results:
-        cache[f"/set_items/{r.id}"] = r
-    cache["/set_items"] = set_results
+        for key, query, model in queries:
+            rows = await self.session.exec(query)
+            models = [model.model_validate(r, from_attributes=True) for r in rows]
+            for m in models:
+                self.store[f"{key}/{m.id}"] = m
+            self.store[key] = models
 
 
-async def load_sets(session, cache):
-    results = (await session.exec(select(Set))).all()
-    set_results = [SetRead.model_validate(r, from_attributes=True) for r in results]
-
-    for r in set_results:
-        cache[f"/sets/{r.id}"] = r
-    cache["/sets"] = set_results
+# cache = Cache()
